864c49b4cd07f133581cd7ae132a3dd472b21426
Ahmed <ahmedkhan895.ak@gmail.com>
2023-09-27 15:57:22 +0100

    changed so gameboard is added in HTML instead

--- main.js
+++ main.js
@@ -1,29 +1,15 @@
-const gameboard = (() => {
 
-    const boardContainer = document.querySelector('.gameboard');
-    let gameArray = ['','','','','','','','','']; //initializes gameArray, each element being one of the 9 tic tac toe elements
+const game = (() => {
 
-
-    function makeBoard(){ //prints gameArray to board
-    gameArray.forEach((n) => {
-        let box = document.createElement('div');
-        box.classList.add('box');
-        box.innerHTML = n;
-        boardContainer.appendChild(box);
-    })};
-
-    return {
-        gameArray,
-        makeBoard,
-    }
 })();
 
 
 const playerFactory = (playerName, marker) => {
-    const sayHello = () => console.log ('hello ' + marker);
-
-    return {playerName, marker, sayHello};
+    return {playerName, marker};
 };
+
+
+
 
 
 
